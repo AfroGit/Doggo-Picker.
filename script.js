3ad4d85dd0c704bb04a09524cd30bf23e72749ce
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-17 03:11:54 +0000

    To drill down on an API and show its values on the dom we use JS to create an element in the HTML. Ln 15

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ fetch(BREEDS_URL)
    .then(data => {
      const breedsObject = data.message;//Turns message into object.
      const breedsArray = Object.keys(breedsObject);//Turns object to array of keys
+     for(let i = 0; i < breedsArray.length; i++){
+       
+     const option = document.createElement('option')//<option></option>
+           option.value = breedsArray[i]//<option value = 'breed'>
+           option.innerText = breedsArray[i]
+     
+     }
+
+     
      // console.log(breedsObject);
      console.log(breedsArray);
    })
